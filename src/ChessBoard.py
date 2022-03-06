3b6dde07fad11417a0bdc45be312dff4080b4218
jas0ndiamond <jason.a.diamond@gmail.com>
2022-03-06 01:26:32 -0800

    helper functions. tostring implementation

--- src/ChessBoard.py
+++ src/ChessBoard.py
@@ -8,7 +8,30 @@ class ChessBoard(ClusterJobResult):
         self.xdim = xdim
         self.ydim = ydim
         
+        self.boardState = "0" * ( self.xdim * self.ydim)
+        
+    def getBoardState(self):
+        return self.boardState;
+        
+    def setBoardState(self, newState):
+        #TODO: dimension check
+        self.boardState = newState
+
+    def getSpace(self, x, y):
         pass
 
     def toString(self):
-        return "I am a Chess Board %d x %d" % (self.xdim, self.ydim)
+        #return "I am a Chess Board %d x %d" % (self.xdim, self.ydim)
+        state = ""
+        
+        for y in range(self.ydim):
+            
+            state += "\n|"
+            
+            row = ""
+            for x in range(self.xdim):
+                row += " " + self.getSpace(x,y) + " |"
+                
+            state += "\n"
+            
+            

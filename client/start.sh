31b2976edc177069065e52591f94da1c83b33364
jas0ndiamond <jason.a.diamond@gmail.com>
2023-07-08 17:24:50 -0700

    workable container for dispynode.py

---
+++ client/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -n "Starting dispynode.py..."
+
+#must run with --daemon or else docker will kill it
+
+/home/dispy/venv_dispy/bin/python3 /home/dispy/venv_dispy/bin/dispynode.py\
+ -d\
+ --cpus $(/bin/cat /proc/cpuinfo | /bin/grep processor | /usr/bin/wc -l)\
+ --dest_path_prefix /home/dispy/node_tmp\
+ --name $DISPY_NODE_NAME\
+ -i $(/bin/hostname -I)\
+ --clean\
+ --msg_timeout 12000\
+ -s $DISPY_SECRET\
+ --ext_host $HOST_IP\
+ --daemon
+ 
+echo "Completed"
+
+sleep infinity
